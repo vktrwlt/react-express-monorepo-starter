f240f757b4c4d2967e06b99cac66d6e15d5abd50
Victor Tsang <victorwltsang@gmail.com>
2022-02-07 17:19:26 -0800

    updated routers

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,14 +5,19 @@ function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("/hello");
+      const response = await fetch("/api/message");
       const json = await response.json();
       setMessage(json.data);
     };
 
     fetchData().catch(console.error);
   }, []);
-  return <div className="App">{message}</div>;
+  return (
+    <div className="App">
+      <p>hello from React</p>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default App;
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const messageRouter = require("./routers/messageRouter");
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/hello", (req, res) => {
-  res.send({
-    data: "Hello from Express",
-  });
-});
+app.use("/api", messageRouter);
 
 app.listen(port, () =>
   console.log(`Server is running on port: http://localhost:${port}`)
